072817182f18b003473dc0faf38f13fcc3930149
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2025-09-28 18:20:21 -0300

    4. Testando TestCase

---
+++ test_spy.py
@@ -0,0 +1,21 @@
+from test_case import *
+
+class TestSpy(TestCase):
+    def __init__(self, name):
+        super().__init__(name)
+        self.was_run = False
+        self.was_set_up = False
+        self.was_tear_down = False
+        self.log = ""
+
+    def set_up(self):
+        self.was_set_up = True
+        self.log += "set_up "
+
+    def test_method(self):
+        self.was_run = True
+        self.log += "test_method "
+
+    def tear_down(self):
+        self.was_tear_down = True
+        self.log += "tear_down"
